71ffaac9ae917f00ce51d922b5e91ccfced23b09
faaris kaber <faariskaber@gmail.com>
2026-05-01 13:14:12 -0400

    Add: two-integer-sum - submission-27

---
+++ Data Structures & Algorithms/two-integer-sum/submission-27.py
@@ -0,0 +1,25 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        paired = []
+
+        for i in range(len(nums)):
+            paired.append( (nums[i], i) )
+
+        copy = sorted(paired)
+
+        left = 0
+        right = len (nums) - 1
+
+        while ( copy[left][0] + copy[right][0] != target ):
+
+            if (copy[left][0] + copy[right][0] > target):
+                right -= 1
+            elif ( copy[left][0] + copy[right][0] < target):
+                left += 1
+
+
+        return sorted([copy[left][1], copy[right][1]])
+    
+
+            
